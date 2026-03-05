7ae9b50ce69a6685c760e4afa649dc4f00890190
Saad Sayyed <saadsyed950@gmail.com>
2026-03-05 14:44:53 +0530

    Login and Register Ui

--- frontend/app/(auth)/auth/sign-up/page.tsx
+++ frontend/app/(auth)/auth/sign-up/page.tsx
@@ -1,5 +1,77 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Register to your new account</CardTitle>
+          <CardDescription>
+            Enter your credentials below to register to your new account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Trevor Santana"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="t@grove.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Confirm Password</Label>
+                </div>
+                <Input id="confirm_password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+          <h6 className="text-center w-full uppercase font-bold text-muted-foreground text-xs">
+            OR
+          </h6>
+          <Button asChild variant="outline" className="w-full">
+            <Link href={"/auth/login"}>Login</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
