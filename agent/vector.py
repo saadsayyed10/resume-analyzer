bb7ce90548d4b96afc791f4a1f67dc2df48ba651
Saad Sayyed <saadsyed950@gmail.com>
2026-03-05 18:31:54 +0530

    Vector embeddings

---
+++ agent/vector.py
@@ -0,0 +1,55 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("./test.csv")
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model = "models/gemini-embedding-001"
+)
+
+db_location = "./chroma_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    documents = []
+    ids = []
+
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=f"""
+            Business Name: {row['BusinessName']}
+            Category: {row['Category']}
+            Area: {row['Area']}
+            Rating: {row['Rating']}
+            Reviews: {row['Reviews']}
+            Opening Hours: {row['OpeningHours']}
+            Services: {row['Services']}
+            Summary: {row['Summary']}
+            """,
+            metadata={
+                "area": row["Area"],
+                "rating": row["Rating"],
+                "reviews": row["Reviews"]
+            }
+        )
+
+        documents.append(document)
+        ids.append(str(i))
+
+vector_store = Chroma(
+    collection_name = "vet_reviews",
+    embedding_function = embeddings,
+    persist_directory = db_location
+)
+
+if add_documents:
+    vector_store.add_documents(documents = documents, ids = ids)
+    
+retriever = vector_store.as_retriever(
+    search_kwargs={
+        "k": 5
+    }
+)
