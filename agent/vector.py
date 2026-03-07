86efd3061e48bab8f8ba67b7afc3125087d72266
Saad Sayyed <saadsyed950@gmail.com>
2026-03-07 23:30:54 +0530

    Agent API changed

--- agent/vector.py
+++ agent/vector.py
@@ -1,12 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-# from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
-import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import shutil
+import os
 
-# df = pd.read_csv("./test.csv")
-loader = PyPDFLoader("./Saad_SWE_Resume.pdf")
+loader = PyPDFLoader("./SaadUbiResume.pdf")
 documents = loader.load()
 
 embeddings = GoogleGenerativeAIEmbeddings(
@@ -14,26 +13,26 @@ embeddings = GoogleGenerativeAIEmbeddings(
 )
 
 db_location = "./chroma_db"
-add_document = not os.path.exists(db_location)
+
+# delete old vector database
+if os.path.exists(db_location):
+    shutil.rmtree(db_location)
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 800,
-    chunk_overlap = 100
+    chunk_size=800,
+    chunk_overlap=100
 )
 
 chunks = text_splitter.split_documents(documents)
 
 vector_store = Chroma(
-    collection_name = "resume_chunks",
-    embedding_function = embeddings,
-    persist_directory = db_location
+    collection_name="resume_chunks",
+    embedding_function=embeddings,
+    persist_directory=db_location
 )
 
-if add_document:
-    vector_store.add_documents(chunks)
+vector_store.add_documents(chunks)
 
 retriever = vector_store.as_retriever(
-    search_kwargs = {
-        "k": 5
-    }
+    search_kwargs={"k": 5}
 )
