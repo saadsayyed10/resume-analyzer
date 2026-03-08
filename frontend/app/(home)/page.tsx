e1c5f84e3651fbe33b9af00050f4553bf3616b7b
Saad Sayyed <saadsyed950@gmail.com>
2026-03-08 18:24:02 +0530

    UI table working for history fetching

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import FetchResumeHistory from "../_components/FetchResumeHistory";
+import { LogOut } from "lucide-react";
 
 const Home = () => {
   const router = useRouter();
@@ -24,7 +25,14 @@ const Home = () => {
     <>
       {token ? (
         <div className="flex justify-center items-center w-full flex-col gap-y-10">
-          <Button onClick={logout}>Logout</Button>
+          <div className="flex justify-between items-center w-full px-10 py-6">
+            <h1 className="font-light text-2xl">
+              <span className="font-medium text-4xl">D</span>elphina
+            </h1>
+            <Button variant={"ghost"} size={"lg"} onClick={logout}>
+              <LogOut />
+            </Button>
+          </div>
           <FetchResumeHistory />
         </div>
       ) : (
--- frontend/app/_components/FetchResumeHistory.tsx
+++ frontend/app/_components/FetchResumeHistory.tsx
@@ -3,8 +3,21 @@
 import { fetchResumeHistoryAPI } from "@/api/resume.api";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
-import { Info, UploadCloud } from "lucide-react";
+import { Ellipsis, Info, Loader2, UploadCloud } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface ResumeHistory {
@@ -19,33 +32,99 @@ const FetchResumeHistory = () => {
   const [data, setData] = useState<ResumeHistory[]>([]);
   const [total, setTotal] = useState(0);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const getHistory = async () => {
+      setLoading(true);
       try {
         const res = await fetchResumeHistoryAPI(token!);
         setData(res.data.data);
         setTotal(res.data.total);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     getHistory();
   }, []);
 
+  const tableHeadings = [
+    {
+      name: "Sr. No",
+    },
+    {
+      name: "Job Description",
+    },
+    {
+      name: "Analyzed Result",
+    },
+    {
+      name: "Date",
+    },
+    {
+      name: "Options",
+    },
+  ];
+
   return (
-    <div className="flex justify-center items-center w-full flex-col gap-y-4 px-20">
+    <div className="flex justify-center items-center w-full flex-col gap-y-4 px-48 mt-10">
       <div className="flex justify-between items-center w-full">
-        <div className="flex justify-start items-center w-full flex-row gap-x-4">
+        <div className="flex justify-start items-center w-full flex-row gap-x-2">
           <Input className="w-104" placeholder="Search analyze history" />
-          <Info className="w-6 h-6 stroke-1 opacity-60" />
+          <Tooltip>
+            <TooltipTrigger>
+              <Info className="w-4 h-4 stroke-1 opacity-60" />
+            </TooltipTrigger>
+            <TooltipContent>Total: {total}</TooltipContent>
+          </Tooltip>
         </div>
         <h1>
-          <Button size={"lg"}>
+          <Button size={"lg"} className="flex items-center">
+            <span>Analyze Resume</span>
             <UploadCloud />
           </Button>
         </h1>
       </div>
+      {!loading ? (
+        <Table>
+          <TableHeader className="bg-neutral-800 border">
+            <TableRow>
+              {tableHeadings.map((tH, idx) => (
+                <TableHead key={idx} className="text-neutral-100">
+                  {tH.name}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody className="border">
+            {data.map((resume, idx) => (
+              <TableRow
+                className={`${idx % 2 === 0 && "bg-neutral-100"}`}
+                key={resume.id}
+              >
+                <TableCell className="font-medium">{idx + 1}</TableCell>
+                <TableCell className="truncate max-w-40">
+                  {resume.job_description}
+                </TableCell>
+                <TableCell className="truncate max-w-40">
+                  {resume.response}
+                </TableCell>
+                <TableCell>{resume.created_at.split("T")[0]}</TableCell>
+                <TableCell className="cursor-pointer">
+                  <Ellipsis className="stroke-[1.5] w-5 h-5" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <div className="flex justify-center items-center w-full">
+          <Loader2 className="w-10 h-10 animate-spin" />
+        </div>
+      )}
     </div>
   );
 };
