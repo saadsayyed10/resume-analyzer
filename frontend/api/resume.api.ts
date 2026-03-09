694e09c820325c0be090c726801f7af84a6579ce
Saad Sayyed <saadsyed950@gmail.com>
2026-03-09 15:50:18 +0530

    Analyze resume frontend API

--- frontend/api/resume.api.ts
+++ frontend/api/resume.api.ts
@@ -8,3 +8,18 @@ export const fetchResumeHistoryAPI = async (token: string) => {
     },
   });
 };
+
+export const analyzeResumeAPI = async (
+  job_description: string,
+  token: string,
+) => {
+  return await axios.post(
+    `${API_URL}/resume/analyze`,
+    { job_description },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+};
